363f88087c4ca49e644e09560c4f321f84bcc400
pedromelomultibrico-sketch <pedromelomultibrico@gmail.com>
2026-09-17 23:16:16 +0100

    Importações para a lista de ficheiros [skip ci]

--- nucleo/java/PonteNucleo.java
+++ nucleo/java/PonteNucleo.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
+import android.content.pm.PackageManager;
 import android.util.Base64;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
