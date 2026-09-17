7fc110d552879bcb8e9cfc89f3cfcd27c7b64966
pedromelomultibrico-sketch <pedromelomultibrico@gmail.com>
2026-09-17 22:03:19 +0100

    Exportar a procura do ecrã sempre ligado [skip ci]

--- web/hwt.js
+++ web/hwt.js
@@ -291,5 +291,5 @@
     return btoa(s);
   }
 
-  window.HWT = { abrir, lerImagens, descodificar, paraDataURL, indiceFundo, construir, codificarExato, paraBase64, opacidade };
+  window.HWT = { abrir, lerImagens, descodificar, paraDataURL, indiceFundo, indiceAod, construir, codificarExato, paraBase64, opacidade };
 })();
